e627e74818fefcd61423d576049c1745bb766cf3
JosePr131 <33135295+JosePr131@users.noreply.github.com>
2019-04-25 07:57:37 -0700

    Create testChart3.java

---
+++ testChart3.java
@@ -0,0 +1,34 @@
+package lastProj;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class testChart3 {
+
+	/*@Test
+	public void test9() 
+	{
+		assertEquals(lastProj.test5(), "Test Success!");
+	}
+	
+	@Test
+	public void test10() 
+	{
+		assertEquals(lastProj.test9(), "Test Success!");
+	}
+	
+	@Test
+	public void test11() 
+	{
+		assertEquals(lastProj.test10(), "Test Success!");
+	}*/
+	
+	//TEST IN CLASS
+	@Test
+	public void test12() 
+	{
+		assertEquals(lastProj.test11(), "Test Success!");
+	}
+
+}
