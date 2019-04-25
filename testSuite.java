00bb863572688c7386ad3cce6d81d202b613964f
JosePr131 <33135295+JosePr131@users.noreply.github.com>
2019-04-25 07:56:31 -0700

    Create testSuite.java
    Create the test Suite

---
+++ testSuite.java
@@ -0,0 +1,24 @@
+package lastProj;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+	@RunWith(Suite.class)
+	@SuiteClasses({
+
+		testChart1.class,
+		testChart2.class,
+		testChart3.class,
+		testChart4.class
+
+	})
+
+
+	public class testSuite 
+	{
+		
+		
+	}
